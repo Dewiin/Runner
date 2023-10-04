53a919934a0d0882dda8386cd3313e15fc63f3a7
Devin <84913029+Dewiin@users.noreply.github.com>
2023-10-04 12:04:50 -0400

    beginning

--- tetris.py
+++ tetris.py
@@ -1,2 +1,17 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1000,700))
+
+while True:
+    for tails in pygame.event.get():
+        if tails.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+
+
 
 
