92702985f440183ae819d5af707deab0d627d85e
Devin <84913029+Dewiin@users.noreply.github.com>
2023-10-05 14:18:27 -0400

    scrolling updated

--- LEPPY.py
+++ LEPPY.py
@@ -12,16 +12,16 @@ background_surface1 = pygame.image.load("Forest/Layers/back.png").convert_alpha(
 background_surface2 = pygame.image.load("Forest/Layers/far.png").convert_alpha()
 background_surface3 = pygame.image.load("Forest/Layers/middle.png").convert_alpha()
 
-#background_surface = pygame.transform.scale(background_surface, (960, 540))
+background_surface1 = pygame.transform.scale(background_surface1, (432, 720))
+background_surface2 = pygame.transform.scale(background_surface2, (528, 720))
 
 ground_surface = pygame.image.load("tileset/ground2.png").convert()
 ground_surface = pygame.transform.scale(ground_surface, (128,128))
 ground_rect = ground_surface.get_rect(bottomleft = (0, SCREEN_HEIGHT))
 
-#player_x = 100
-#player_surface = pygame.image.load("characters/walking1.png").convert_alpha()
-#player_surface = pygame.transform.scale(player_surface, (64,64))
-#player_rect = player_surface.get_rect(midbottom = (player_x, 410))
+player_surface = pygame.image.load("characters/walking1.png").convert_alpha()
+player_surface = pygame.transform.scale(player_surface, (96,96))
+player_rect = player_surface.get_rect(midbottom = (100, 560))
 
 bg_width = background_surface1.get_width()
 ground_width = ground_surface.get_width()
@@ -51,9 +51,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    #player_rect.x += 4
-    #if(player_rect.left > 1000) : player_rect.right = 0
-    #screen.blit(player_surface, player_rect)
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     
